02ba76548af230a0464d3b0a53d77f4debc22610
voidzero <kartikjagdale11@gmail.com>
2016-12-19 18:39:26 +0530

    Suggestion Router and Component

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,6 @@ class List
   include Mongoid::Document
   field :name,            type: String
   field :description,     type: String, default: ""
-  
+  field :image_url,       type: String, default: "https://avatars2.githubusercontent.com/u/210414?v=3&s=400"  
   has_and_belongs_to_many :members
 end
