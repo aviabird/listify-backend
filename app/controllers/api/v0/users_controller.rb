eeb992b72ac7ea6a7c5d5e0948def2174483a6bb
voidzero <kartikjagdale11@gmail.com>
2016-12-26 16:34:07 +0530

    uncomment code

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -3,16 +3,16 @@ module Api::V0
     before_action :authenticate_user!
 
     def create_list
-      # list = List.includes(:members).find(params["id"])
+      list = List.includes(:members).find(params["id"])
       
-      # access_token = current_user.access_tokens["twitter"]
-      # secret_token =  current_user.secret_tokens["twitter"]
+      access_token = current_user.access_tokens["twitter"]
+      secret_token =  current_user.secret_tokens["twitter"]
 
-      # service_params = { access_token: access_token, secret_token: secret_token }
+      service_params = { access_token: access_token, secret_token: secret_token }
 
-      # @twitter_create_list = TwitterApi::CreateList.new(params: service_params)
-      # result = @twitter_create_list.call(list: list, user: current_user)
-      result = { status: true, new_user_list: current_user.users_lists.first}
+      @twitter_create_list = TwitterApi::CreateList.new(params: service_params)
+      result = @twitter_create_list.call(list: list, user: current_user)
+      # result = { status: true, new_user_list: current_user.users_lists.first}
       render json: result
     end
 
