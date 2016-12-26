eb4068a7baafca953c07ce88fbf90ad927d19d15
voidzero <kartikjagdale11@gmail.com>
2016-12-26 12:29:24 +0530

    remove request foregery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :null_session
   before_action :set_current_user
 
   # Override Devise authenticate method 
