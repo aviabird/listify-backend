50475172dc99b9991e11d90f981f8285fec8afbb
voidzero <kartikjagdale11@gmail.com>
2017-01-20 10:20:45 +0530

    fix bug in fav and retweet feature

--- app/controllers/api/v0/tweets_controller.rb
+++ app/controllers/api/v0/tweets_controller.rb
@@ -3,32 +3,32 @@ module Api::V0
     before_action :authenticate_user!    
 
     def add_tweet_to_fav
-      tweetId = params["feedId"];
+      tweet = params["feed"];
       access_token = current_user.access_tokens["twitter"]
       secret_token =  current_user.secret_tokens["twitter"]
       service_params = { access_token: access_token, secret_token: secret_token}
       @tweet_service = TwitterApi::TweetService.new(params: service_params)
-      result = @tweet_service.add_to_fav(tweetId);
+      result = @tweet_service.add_to_fav(tweet);
       render json: result
     end
 
     def remove_tweet_from_fav
-      tweetId = params["feedId"];
+      tweet = params["feed"];
       access_token = current_user.access_tokens["twitter"]
       secret_token =  current_user.secret_tokens["twitter"]
       service_params = { access_token: access_token, secret_token: secret_token}
       @tweet_service = TwitterApi::TweetService.new(params: service_params)
-      result = @tweet_service.remove_tweet_from_fav(tweetId);
+      result = @tweet_service.remove_tweet_from_fav(tweet);
       render json: result
     end
 
     def retweet
-      tweetId = params["feedId"];
+      tweet = params["feed"];
       access_token = current_user.access_tokens["twitter"]
       secret_token =  current_user.secret_tokens["twitter"]
       service_params = { access_token: access_token, secret_token: secret_token}
       @tweet_service = TwitterApi::TweetService.new(params: service_params)
-      result = @tweet_service.retweet(tweetId);
+      result = @tweet_service.retweet(tweet);
       render json: result
     end
 
--- app/services/twitter_api/tweet_service.rb
+++ app/services/twitter_api/tweet_service.rb
@@ -1,33 +1,37 @@
 module TwitterApi
   class TweetService < TwitterApi::Base
-    def add_to_fav(tweet_id)
+    def add_to_fav(tweet)
       begin
-        res = @client.favorite!([tweet_id])
+        user_list_id = tweet[:user_list_id]
+        res = @client.favorite!([tweet])
+        tweets = add_user_list_id_to_tweets(user_list_id, res)
         # Return fav tweet
-        return { status: true, feed: res.first };
+        return { status: true, feed: tweets };
       rescue => e
         return { status: false, error: e }
       end
     end
 
-    def remove_tweet_from_fav(tweet_id)
+    def remove_tweet_from_fav(tweet)
       begin
-        # binding.pry
-        res = @client.unfavorite([tweet_id])
+        user_list_id = tweet[:user_list_id]
+        res = @client.unfavorite([tweet])
+        tweets = add_user_list_id_to_tweets(user_list_id, res)
         # Return tweet
-        return { status: true, feed: res.first };
+        return { status: true, feed: tweets };
       rescue => e
         return { status: false, error: e }
       end
     end
 
-    def retweet(tweet_id)
+    def retweet(tweet)
       begin
-        # binding.pry
-        res = @client.retweet([tweet_id])
+        user_list_id = tweet[:user_list_id]
+        res = @client.retweet([tweet])
         retweet_status = res.first.to_hash[:retweeted_status]
+        retweeted_tweets = add_user_list_id_to_tweets(user_list_id, [retweet_status])
         # Return retweeted tweet
-        return { status: true, feed: retweet_status }
+        return { status: true, feed: [retweeted_tweets] }
       rescue => e
         return { status: false, error: e }
       end
@@ -42,5 +46,13 @@ module TwitterApi
         return {status: false, error: e}
       end
     end
+
+
+    private
+    
+      def add_user_list_id_to_tweets(user_list_id, tweets)
+        tweets.map!{|tweet| tweet.to_hash.merge(user_list_id: userListId)}
+        tweets
+      end
   end
 end
